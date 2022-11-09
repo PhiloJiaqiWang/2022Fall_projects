cc28259bb08710081b4bd8a1bae5b84fa440c08e
YvonneZ <yvonnezhouyf@berkeley.edu>
2022-11-09 16:33:45 -0600

    several monster class added. monster generation algo modification pending

--- game_logic.py
+++ game_logic.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import random
 
+
 @dataclass
 class Equipment:
 
@@ -116,7 +117,6 @@ class Player:
         self.player_health_energy = health_energy_lis
 
 
-
 player1 = Player()
 print(player1.player_health_energy)
 a = 'Sneakers'
@@ -125,6 +125,8 @@ print(equ.damage_min)
 player1.set_player_equipments([a, '', '', ''])
 player1.set_player_att()
 print(player1.player_att)
+
+
 class Rock():
     """
     everytime the player crack rocks, it will generate different items, you can find the information here:
@@ -151,11 +153,141 @@ class Rock():
         number = round(random.uniform(min_num, max_num), 0)
         return number
 
-class Monster:
+
+############
+# Monsters #
+############
+class Monster(object):
     """
     the monsters information is here https://stardewvalleywiki.com/Monsters. It may need to use inheritance.
     You can try to create a relatively complicated one first.
     """
+    killable, HP, damage, defense, speed, XP, drop_rate = True, 0, 0, 0, 0, 0, {}
+
+    def __init__(self, level, place=(0, 0)):
+        """
+        Create a monster with its name.
+        """
+        self.level = level
+        self.place = place
+        self.if_bottom()
+
+    def if_bottom(self):
+        result = self.level == 120
+        if result:
+            self.drop_rate = {"Diamond": 0.0005, "Prismatic Shard": 0.0005}
+        return result
+
+
+class Slime(Monster):
+    """
+    A Slime is one of the monster categories. It has several specifications including GreenSlime, FrostJelly, and
+    RedSludge. A Slime itself is not a kind of monster that will show up in the Mines.
+    """
+    speed = 2
+
+
+class GreenSlime(Slime):
+    """A GreenSlime is a kind of Slime that could be found on any level in the Mines."""
+    HP, damage, defense, XP = 24, 5, 1, 3
+
+    def drop(self):
+        self.drop_rate.update({"Amethyst": 0.015, "Dwarf Scroll I": 0.005, "Dwarf Scroll II": 0.001, "Green Algae": 0.1,
+                               "Sap": 0.15, "Slime": 0.8})  # TODO: sum != 1; Slime Hutch?
+
+
+class FrostJelly(Slime):
+    """A FrostJelly is a kind of Slime that could only be found on level 41-79 in the Mines."""
+    # TODO: can sometimes be found on other floors? sr. https://stardewvalleywiki.com/Slimes
+    HP, damage, defense, XP = 106, 7, 0, 6  # TODO: the attack might grow
+
+    def drop(self):
+        self.drop_rate.update({"Dwarf Scroll II": 0.005, "Dwarf Scroll III": 0.015, "Dwarf Scroll IV": 0.001,
+                               "Jade": 0.02, "Sap": 0.5, "Slime": 0.75, "Winter Root": 0.08})
+        # TODO: sum != 1; Slime Hutch?
+
+
+class RedSludge(Slime):
+    """A GreenSlime is a kind of Slime that could be found on any level in the Mines."""
+    HP, damage, defense, XP = 205, 16, 0, 10
+
+    def drop(self):
+        self.drop_rate.update({"Coal": 0.01, "Dwarf Scroll III": 0.005, "Dwarf Scroll II": 0.001, "Green Algae": 0.1,
+                               "Sap": 0.5, "Slime": 0.8, "White Algae": 0.1})  # TODO: sum != 1; Slime Hutch? 1-3 Coal
+        self.drop_rate["Diamond"] = 0.01
+
+
+class Bug(Monster):
+    """
+    A Bug is a kind of monster that fly up and down or left and right in a fixed path. They will ignore the Player
+    and will only follow the same path until slain. They only appear on floor 1-39.
+    """
+    HP, damage, defense, speed, XP = 1, 8, 0, 2, 1
+
+    def drop(self):
+        self.drop_rate.update({"Ancient Seed": 0.005, "Bug Meat": 0.76, "Dwarf Scroll I": 0.005,
+                               "Dwarf Scroll IV": 0.001, "White Algae": 0.02})
+        # TODO: sum != 1
+
+
+class Bat(Monster):
+    """
+    A Bat is one of the monster categories. It has several variations including FrostBat and LavaBat. Bat itself is
+    also a monster that will appear. It only appears on floor 31-39.
+    """
+    HP, damage, defense, speed, XP = 24, 6, 1, 3, 3
+
+    def drop(self):
+        self.drop_rate.update({"Bat Wings": 0.94, "Bomb": 0.02, "Dwarf Scroll I": 0.005,
+                               "Dwarf Scroll IV": 0.001, "Rare Disc": 0.01})
+        # TODO: sum != 1, 1-2 Bat Wings
+
+
+class FrostBat(Bat):
+    """A FrostBat is a variation of Bat and could only be found on level 41-79 in the Mines."""
+    HP, damage, XP = 36, 7, 7
+
+    def drop(self):
+        self.drop_rate.update({"Bat Wings": 0.95, "Bomb": 0.02, "Dwarf Scroll II": 0.005,
+                               "Dwarf Scroll IV": 0.001, "Rare Disc": 0.01})
+        # TODO: sum != 1, 1-2 Bat Wings
+
+
+class LavaBat(Bat):
+    """A LavaBat is a variation of Bat and could only be found on level 81-119 in the Mines."""
+    HP, damage, XP = 80, 15, 15
+
+    def drop(self):
+        self.drop_rate.update({"Bat Wings": 0.97, "Bomb": 0.02, "Dwarf Scroll III": 0.005,
+                               "Dwarf Scroll IV": 0.001, "Rare Disc": 0.01})
+        # TODO: sum != 1, 1-2 Bat Wings
+
+
+class Crab(Monster):
+    """
+    A Crab is one of the monster categories. It has several variations including RockCrab and LavaCrab. A Crab itself
+    is not a kind of monster that will show up in the Mines.
+    """
+    pass
+
+
+class RockCrab(Crab):
+    """A RockCrab is a variation of Crab and could only be found on level 1-29 in the Mines."""
+    HP, damage, defense, speed, XP = 30, 5, 1, 2, 4
+
+    def drop(self):
+        self.drop_rate.update({"Cherry Bomb": 0.4, "Crab": 0.15, "Dwarf Scroll I": 0.005, "Dwarf Scroll IV": 0.001})
+        # TODO: sum != 1
+
+
+class LavaCrab(Crab):
+    """A LavaCrab is a variation of Crab and could only be found on level 80-119 in the Mines."""
+    HP, damage, defense, speed, XP = 120, 15, 3, 3, 12
+
+    def drop(self):
+        self.drop_rate.update({"Bomb": 0.4, "Crab": 0.25, "Dwarf Scroll III": 0.005, "Dwarf Scroll IV": 0.001})
+        # TODO: sum != 1
+
 
 class Floor:
     """
@@ -165,6 +297,7 @@ class Floor:
     floor_containers = []  # the list of the rocks in this floor
     floor_monsters = []  # the list of the monsters
     total_value = 0  # total value of all the rocks in this floor
+
     def __init__(self, level):
         self.r = pd.read_csv('rock.csv')
         self.r.set_index("item", inplace=True)
@@ -182,6 +315,7 @@ class Floor:
         item_list = self.r.index.tolist()
         output = random.choices(item_list, weights=possibility_list, k=1)
         return output
+
     # @staticmethod
     # generate the list of rock and the total value in this floor
     def generate_rock_list(self):
@@ -201,7 +335,7 @@ class Floor:
         """
         monsters_num = random.randint(5, 10)
         for i in range(0, monsters_num):
-            monster = Monster(self.level)
+            monster = Monster(self.level)  # TODO: monster generation algo modification
             self.floor_monsters.append(monster)
 
 
@@ -209,10 +343,12 @@ aa, bb = Floor(44).generate_rock_list()
 print('floor container', aa)
 print('total value in this floor', bb)
 
+
 class MainGame:
     """
     running the game
     """
+
     def __init__(self, level_start, level_end):
         pass
 
